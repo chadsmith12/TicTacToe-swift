47b554cae940cc032e4a6b7afb0e909d23064e8d
Chad Smith <smith.chad12@gmail.com>
2021-08-25 22:27:09 -0500

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Chad Smith on 8/25/21.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
